e9b0a9404c3c4988abb4d4cdf20433f5826d6e13
ceal2013 <carlos.ortiz51@inacapmail.cl>
2025-07-22 02:50:39 -0400

    Add homepage template and logo image for HotelNew application
    - Created a new HTML template for the homepage with a responsive layout.
    - Integrated Bootstrap for styling and layout management.
    - Added a sidebar navigation menu with sections for Rooms, Passengers, Reservations, and Users.
    - Included user session management with a logout button.
    - Added a logo image for the HotelNew application.

--- HotelNewApp/views.py
+++ HotelNewApp/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import LoginForm
 
 # Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Credenciales inválidas')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
