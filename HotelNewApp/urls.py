0b277b809ac6c16913776d2f614592b2cbe8bd48
ceal2013 <carlos.ortiz51@inacapmail.cl>
2025-07-23 12:24:19 -0400

    Añade rutas para la gestión de habitaciones, pasajeros y reservas en la aplicación HotelNew.

--- HotelNewApp/urls.py
+++ HotelNewApp/urls.py
@@ -11,4 +11,17 @@ urlpatterns = [
     path('usuarios/crear/', views.crear_usuario, name='crear_usuario'),
     path('usuarios/editar/<int:id_usuario>/', views.editar_usuario, name='editar_usuario'),
     path('usuarios/eliminar/<int:id_usuario>/', views.eliminar_usuario, name='eliminar_usuario'),
+    # Habitaciones
+    path('habitaciones/', views.listar_habitaciones, name='listar_habitaciones'),
+    path('habitaciones/crear/', views.crear_habitacion, name='crear_habitacion'),
+    path('habitaciones/eliminar/<int:id_habitacion>/', views.eliminar_habitacion, name='eliminar_habitacion'),
+
+    # Pasajeros
+    path('pasajeros/', views.listar_pasajeros, name='listar_pasajeros'),
+    path('pasajeros/crear/', views.crear_pasajero, name='crear_pasajero'),
+
+    # Reservas
+    path('reservas/', views.listar_reservas, name='listar_reservas'),
+    path('reservas/crear/', views.crear_reserva, name='crear_reserva'),
+    path('reservas/cancelar/<int:id_reserva>/', views.cancelar_reserva, name='cancelar_reserva'),
 ]
